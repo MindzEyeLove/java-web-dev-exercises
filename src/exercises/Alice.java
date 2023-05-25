02a3e1f9471731045c0f9272aab969691bb2ced0
Harmony Love <harmonylove427@gmail.com>
2023-05-24 20:12:17 -0500

    initial commit

---
+++ src/exercises/Alice.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class Alice {
+    public static void main(String[] args) {
+//        String message = Message.getMessage("fr");
+//        System.out.println(message);
+    }
+}
---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.studios.areaofacircle;
+
+public class Circle {
+
+        public static double getArea(double radius) {
+            return Math.PI * radius * radius;
+        }
+    }
+
