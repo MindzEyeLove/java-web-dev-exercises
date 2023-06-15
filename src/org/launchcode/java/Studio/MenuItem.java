8617f5b6d48b3362ff7cffc9b6ca72764ea33ecf
Harmony Love <harmonylove427@gmail.com>
2023-06-14 19:28:50 -0500

    restaurant menu studio initial commit

---
+++ src/org/launchcode/java/Studio/MenuItem.java
@@ -0,0 +1,15 @@
+package org.launchcode.java.Studio;
+
+public class menuItem(){
+
+
+  public MenuItem(String name, double price, String description, String category) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+    }
+}
+System.out.println(MenuItem("pizza", 8.09, "Cheesy", "carbs"))
+
+
